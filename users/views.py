791db6f6207c8dabd5dce8b12c165599e4ffef28
vna15 <vnazevedo08@hotmail.com>
2024-06-05 18:15:41 -0300

    init model class

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Users
+#from .serializers import UsersSerializer
+
+
+# CRUD user
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def user_manager(request):
+    pass
